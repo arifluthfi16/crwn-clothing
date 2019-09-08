1ac8bd818f9709007ba4b59d25c9c696c8e7cb13
Arif Luthfi <arifluthfi16@gmail.com>
2019-09-08 22:34:24 +0700

    Update

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,14 +2,12 @@ import React from 'react';
 
 import Directory from '../../components/directory/directory.component';
 
-import './homepage.style.scss';
+import './homepage.styles.scss';
 
-const HomePage = () => {
-    return(
-      <div className="homepage">
-        <Directory/>
-      </div>
-    );
-}
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
 
 export default HomePage;
