ea789b3bd5a89ea9dbf39d546276a015323bff7f
Arif Luthfi <arifluthfi16@gmail.com>
2019-09-09 13:59:40 +0700

    Updates

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
+import Header from './components/header/header.component';
 
 import './App.css';
 
@@ -9,10 +10,11 @@ import ShopPage from "./pages/shop/shop.component";
 function App() {
   return (
     <div>
-      <Switch>
-        <Route exact path='/' component={HomePage} />
-        <Route path='/shop' component={ShopPage} />
-      </Switch>
+        <Header/>
+          <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/shop' component={ShopPage} />
+          </Switch>
     </div>
   );
 }
